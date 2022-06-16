cd4982e0185a39a1bbd854d6394201c52bb8b7f2
SangHyeok <tkd1685@gmail.com>
2022-06-16 16:27:02 +0900

    test20220616

--- Final Test().py
+++ Final Test().py
@@ -1,26 +1,29 @@
 import paho.mqtt.client as mqtt
 import RPi.GPIO as GPIO
 
-def on_connect(client, userdata, flag, rc):
-	print("Connected with rc:" + str(rc))
-	client.subscribe("dw")
-
-def on_message(client, userdata, msg):
-	if 2 in msg.payload:
-		print("ON!!!")
-		GPIO.output(11, True)
-	else :
-		print("OFF!!")
-		GPIO.output(11, False)
 
 client = mqtt.Client()
-client.on_connect = on_connect
-client.on_message = on_message
 
-client.connect("localhost", 1883, 60)
+client.connect("192.168.111.92", 1883, 60)
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11, GPIO.OUT)
+GPIO.setup(15, GPIO.OUT)
+GPIO.setwarnings(False)
+
+def on_connect(client, userdata, flag, rc):
+	print("Connected with rc:" + str(rc))
+	client.subscribe("SH")
+
+def on_message(client, userdata, msg):
+	if 7 in msg.payload:
+		print("ON!!!")
+		GPIO.output(15, True)
+	else :
+		print("OFF!!")
+		GPIO.output(15, False)
+
+client.on_connect = on_connect
+client.on_message = on_message
 
 
 client.loop_forever()
